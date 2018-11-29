beb3e9a81c34f76608a1689d944d7a66feda028c
Mary Lark <mary@marylark.com>
2018-11-29 02:08:39 +0000

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,7 +1,6 @@
 const expect = chai.expect;
 
 describe("index.js", () => {
-
   const codes = [
     "ArrowUp",
     "ArrowUp",
@@ -19,31 +18,30 @@ describe("index.js", () => {
     const keyboardEvent = new KeyboardEvent("keydown", { key });
     document.body.dispatchEvent(keyboardEvent);
   }
-  
-  init()
 
   describe("Konami code", () => {
-    
-    const spy = sinon.stub(window, "alert");
-    
     it("triggers an alert if the right code is entered", () => {
-      
+      init();
+
+      var spy = sinon.spy(window.alert);
+
       for (let i = 0, l = codes.length; i < l; i++) {
         triggerKeyDown(codes[i]);
       }
-      expect(spy.called).to.equal(true)
-      expect(spy.callCount).to.equal(1)
+
+      expect(spy.called);
     });
 
     it("does not trigger an alert if the wrong code is entered", () => {
-      spy.reset()
+      init();
 
-      for (let i = 0; i < codes.length - 1; i++) {
-        triggerKeyDown(codes[i])
+      var spy = sinon.spy(window.alert);
+
+      for (let i = 0, l = codes.length; i < l; i++) {
+        triggerKeyDown(i);
       }
-      triggerKeyDown("ArrowUp")
-      
-      expect(spy.notCalled).to.equal(true);
+
+      expect(spy.notCalled);
     });
   });
 });
